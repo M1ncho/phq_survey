1e94c089b93356391c5cf7d45739851d8d5fec4e
jiwan0630 <neve0630@blifeinc.com>
2023-08-02 10:36:50 +0900

    sendresult 공유

---
+++ phqsurvey_ios/Entity/SendResult.swift
@@ -0,0 +1,24 @@
+//
+//  SendResult.swift
+//  phqsurvey_ios
+//
+//  Created by KJW on 2023/06/09.
+//
+
+import Foundation
+
+class SendResult: Codable {
+    var result: Bool
+    
+    
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        result = try values.decode(Bool.self, forKey: .result)
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case result
+    }
+    
+}
